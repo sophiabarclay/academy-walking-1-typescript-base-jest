66c882f92b64298df20cfcfa03881d3d60c91f42
cihantklc <cihan.toklucu@cazoo.com>
2022-05-20 15:53:25 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/example.test.ts

--- src/main/example.ts
+++ src/main/example.ts
@@ -2,10 +2,9 @@ export const add = (numbers: string) => {
   if (numbers === "") return 0;
 
   if (numbers.length === 1) return parseInt(numbers, 10);
-  
-  if (numbers.length === 3) {
-      return numbers.split(',')
-        .map(num => parseInt(num, 10))
-        .reduce((a, b) => a + b, 0);
-  }
+
+  return numbers.split(',')
+  .map(num => parseInt(num, 10))
+  .reduce((a, b) => a + b, 0);
+
 };
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -9,10 +9,16 @@ describe('string calculator', () => {
   it("should return the number if string includes one number", () => {
     const result = add("1");
     expect(result).toBe(1);
-  })
+  });
 
   it("should return the number if string includes two numbers", () => {
     const result = add("1,2");
     expect(result).toBe(3);
-  })
+  });
+  1
+
+  it.each([["1,2,3", 6], ["1,2,3,4", 10], ["1,2,3,4,5,6,7,8,9,10", 55]])("should handle more than two numbers", (numbers, sum) => {
+    const result = add(numbers);
+    expect(result).toBe(sum);
+  });
 })
