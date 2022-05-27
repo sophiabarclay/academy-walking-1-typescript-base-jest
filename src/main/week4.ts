8bf4b7a9c889c7caf48e9d1fbbfd227d2c9d3ef0
Sophia Barclay <barclay.sophia@gmail.com>
2022-05-27 16:04:40 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/week4.test.ts

--- src/main/week4.ts
+++ src/main/week4.ts
@@ -1,3 +1,6 @@
 export const romanNumeraliser = (number: number): string => {
-  return number === 1 ? "I" : "II";
+  if (number === 1 ) return "I";
+  if (number === 2 ) return "II";
+  if (number === 3 ) return "III";
+  else return "";
 };
--- src/test/week4.test.ts
+++ src/test/week4.test.ts
@@ -5,6 +5,7 @@ describe('roman numerals', () => {
     a    | expected
     ${1} | ${"I"}
     ${2} | ${"II"}
+    ${3} | ${"III"}
   `('returns $expected when given $a as a position', ({a, expected}) => {
     expect(romanNumeraliser(a)).toBe(expected);
   });
