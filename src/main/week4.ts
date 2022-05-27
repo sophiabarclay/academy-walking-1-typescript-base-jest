9a4b04412f952676615af8165ced5f00da5bbb8d
Luke Dawkes <luke.dawkes@cazoo.co.uk>
2022-05-27 16:25:25 +0100

    "mob next [ci-skip]"

--- src/main/week4.ts
+++ src/main/week4.ts
@@ -1,6 +1,18 @@
+const characters = {
+  singles: ["I", "V"],
+  tens: ["X", "L"],
+  hundreds: ["C", "D"],
+  thousands: ["M", "V"],
+}
+
 export const romanNumeraliser = (number: number): string => {
-  if (number === 1 ) return "I";
-  if (number === 2 ) return "II";
-  if (number === 3 ) return "III";
-  else return "";
+  if (number >= 5) {
+    const difference = number - 5;
+
+    return characters.singles[1].padEnd(difference + 1, characters.singles[0])
+  } else if (number === 4) {
+    return characters.singles[0] + characters.singles[1];
+  } else {
+    return characters.singles[0].padEnd(number, characters.singles[0]);
+  }
 };
--- src/test/week4.test.ts
+++ src/test/week4.test.ts
@@ -6,6 +6,11 @@ describe('roman numerals', () => {
     ${1} | ${"I"}
     ${2} | ${"II"}
     ${3} | ${"III"}
+    ${4} | ${"IV"}
+    ${5} | ${"V"}
+    ${6} | ${"VI"}
+    ${7} | ${"VII"}
+    ${8} | ${"VIII"}
   `('returns $expected when given $a as a position', ({a, expected}) => {
     expect(romanNumeraliser(a)).toBe(expected);
   });
